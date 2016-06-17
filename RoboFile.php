1a6768ac523a56e587686ae0f68de17925e50a00
Osushi <tdu.future.i@gmail.com>
2016-06-17 19:52:25 +0900

    Create init task

--- RoboFile.php
+++ RoboFile.php
@@ -2,4 +2,9 @@
 
 class RoboFile extends \Robo\Tasks
 {
+  public function init()
+  {
+    $this->_exec('find log/ -type d -exec chmod 777 {} \;');
+    $this->_exec('find tmp/ -type d -exec chmod 777 {} \;');
+  }
 }
