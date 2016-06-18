6a4a624085d18db7b33e5b25c61e6542860d0a70
Osushi <tdu.future.i@gmail.com>
2016-06-19 06:13:09 +0900

    Clean Route on base

--- config/Routes/Base.php
+++ config/Routes/Base.php
@@ -1,24 +1,5 @@
 <?php
 
 $app->get('/?', function () {
-  $top = new \Controllers\Top();
-  $top->action_show();
-});
-
-$app->post('/?', function () {
-  $top = new \Controllers\Top();
-  $top->action_post();
-});
-
-$app->delete('/?', function () {
-  $top = new \Controllers\Top();
-  $top->action_delete();
-});
-
-$app->notFound(function () use ($app) {
-  \Services\Render::render($app, 'exception/404');
-});
-
-$app->error(function () use ($app) {
-  \Services\Render::render($app, 'exception/500');
+  echo 'Hello Osushi Framework';
 });
